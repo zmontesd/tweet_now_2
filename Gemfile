13f8d07872ffcdb7fb6e252427687be2c87b2d3c
DBC-Apprentice <dbc@dbc.com>
2013-07-29 18:21:24 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source :rubygems
+
+# PostgreSQL driver
+gem 'pg'
+
+# Sinatra driver
+gem 'sinatra'
+
+# Use Thin for our web server
+gem 'thin'
+
+gem 'activesupport'
+gem 'activerecord'
+
+gem 'rake'
+
+gem 'shotgun'
+
+gem 'oauth'
+gem 'twitter'
+
+group :test do
+  gem 'faker'
+  gem 'rspec'
+end
---
+++ config/environment.rb
@@ -0,0 +1,47 @@
+# Set up gems listed in the Gemfile.
+# See: http://gembundler.com/bundler_setup.html
+#      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Require gems we care about
+require 'rubygems'
+
+require 'uri'
+require 'pathname'
+
+require 'pg'
+require 'active_record'
+require 'logger'
+
+require 'sinatra'
+require 'shotgun'
+
+require 'erb'
+
+require 'oauth'
+require 'twitter'
+
+# Some helper constants for path-centric logic
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+
+APP_NAME = APP_ROOT.basename.to_s
+
+# Set up the controllers and helpers
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
+
+# Set up the database and models
+require APP_ROOT.join('config', 'database')
+
+env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+
+env_config.each do |key, value|
+  ENV[key] = value
+end
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['TWITTER_KEY']
+  config.consumer_secret = ENV['TWITTER_SECRET']
+end
