da61793dbb1f95c82645c31a7827fbc519c6603e
zmontesd <zmontesd@gmail.com>
2013-07-31 11:07:18 -0700

    commenting out the yaml env code

--- config/environment.rb
+++ config/environment.rb
@@ -35,11 +35,11 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
-env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+# env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
 
-env_config.each do |key, value|
-  ENV[key] = value
-end
+# env_config.each do |key, value|
+#   ENV[key] = value
+# end
 
 Twitter.configure do |config|
   config.consumer_key = ENV['TWITTER_KEY']
