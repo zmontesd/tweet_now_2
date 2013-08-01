ee332f9b59b59e943888a06cdd4d44a2982b6bbb
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-31 21:00:51 -0700

    remove tweet method from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tweets
 
-  def tweet(status)
-    tweet = tweets.create!(:status => status)
-    TweetWorker.perform_async(tweet.id)
-  end
-
   def tweets_stale?
     ((Time.now - self.tweets.last.created_at) / 60) > 15
   end
